416e9ed5cead6b97f59b28f1a6d9b1a275446571
Sebastian Pinto Becerra <sebastianpintobecerra7@gmail.com>
2024-07-20 14:40:57 -0500

    Finalizacion curso Autenticación con Angular y JWT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@guards/auth.guard';
+import { RedirectGuard } from '@guards/redirect.guard';
+
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [RedirectGuard],
     loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'app',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    canActivate: [RedirectGuard],
+    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
 ];
 
--- src/app/modules/users/pages/users-table/data-source.ts
+++ src/app/modules/users/pages/users-table/data-source.ts
@@ -1,16 +1,16 @@
 import { DataSource } from '@angular/cdk/collections';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-export class DataSourceUser extends DataSource<any[]> {
+export class GenericDataSource<T> extends DataSource<T> {
 
-  data = new BehaviorSubject<any[]>([]);
-  originalData: any[]= [];
+  data = new BehaviorSubject<T[]>([]);
+  originalData: T[] = [];
 
-  connect(): Observable<any[]> {
+  connect(): Observable<T[]> {
     return this.data;
   }
 
-  init(data: any[]) {
+  init(data: T[]) {
     this.originalData = data;
     this.data.next(data);
   }
